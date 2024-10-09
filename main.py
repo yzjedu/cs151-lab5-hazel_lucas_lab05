123304c862d34ad045319f5232e795d14e54dc6a
lapodowski <lapodowski@loyola.edu>
2024-10-09 16:13:48 -0400

    Main.py commit 1

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-# Read the README.md
-# Read it again
-# Your code here
-# Delete these 4 lines of comments
+balance = 1000
+choice = ''
+print("Welcome to the ATM. Enter D to deposit, W to withdraw, V to view balance, or E to exit.")
+while choice not in ['e','d','w','v']:
+    choice = input("You have not entered D, W, V, or E.\nPlease enter D, W, V, or E: ").lower()
+    while choice != 'e':
+        if choice == 'd':
+            amount = float(input("Enter your deposit amount: "))
+            balance += amount
+        elif choice =='w':
+            amount = float(input("Enter your withdrawal amount: "))
+            balance -= amount
+        else:
+            print(f'Your current balance is ${balance:.2f}')
+        choice = input("Enter D, W, V, or E: ").lower()
+print("Thanks for using the ATM. Have a good day!")
